a213910c7b66c5865482b328ddaf06a9785cc5ac
@MarinaGAguirre <maguirre.neticel@gmail.com>
2023-10-04 20:38:24 -0300

    Miercoles 4 de Octubre

---
+++ routes/ultracavitacion.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET webDevelopment page. */
+router.get('/', function(req, res, next) {
+  res.render('ultracavitacion');
+});
+
+module.exports = router;
